adcaa367c424e86afc2aba86b6a28942ab6656fe
Tomer-Banay <tomer.banay@post.runi.ac.il>
2024-11-15 17:37:31 +0200

    2+3

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    
+                String s = args[0];
+                 s = s.toUpperCase();
+                int times = Integer.parseInt (args [1]);
+
+                String an = "AEFHILMNORSX";
+                int i = 0;
+                while (i < s.length()) {
+                char c = s.charAt(i);
+                        if (an.indexOf(c) == -1) {
+                        System.out.println("Give me a " + c + ": " + c + "!");
+                        } else {
+                        System.out.println("Give me an " + c + ": " + c + "!");
+                        }
+                i++;
+                }
+                for (int j = 0; j < times; j++) {
+                        System.out.println( s + "!!!");
+                }
         }
-}
+ }
+
