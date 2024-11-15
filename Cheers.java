33966496e00c60e93716adcbd3119f7b7da5907b
Tomer-Banay <tomer.banay@post.runi.ac.il>
2024-11-15 17:50:24 +0200

    2+3

--- Cheers.java
+++ Cheers.java
@@ -11,7 +11,7 @@ public class Cheers {
                 while (i < s.length()) {
                 char c = s.charAt(i);
                         if (an.indexOf(c) == -1) {
-                        System.out.println("Give me a " + c + ": " + c + "!");
+                        System.out.println("Give me a  " + c + ": " + c + "!");
                         } else {
                         System.out.println("Give me an " + c + ": " + c + "!");
                         }
